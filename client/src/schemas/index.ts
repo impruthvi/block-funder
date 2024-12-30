88386da19a0fbff30427fd3f592512f71f11540a
impruthvi <pruthvirajput97@gmail.com>
2024-12-30 17:51:18 +0530

    add validation schema for campaign creation using Zod and update dependencies

---
+++ client/src/schemas/index.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const createCampaignSchema = z.object({
+  name: z.string().trim().min(1, "Required"),
+  title: z.string().trim().min(1, "Required"),
+  description: z.string().trim().min(1, "Required"),
+  target: z
+    .number()
+    .refine((value) => value > 0, "Target must be greater than zero"), // Ensure positive value
+  deadline: z
+    .date()
+    .refine(
+      (date) => (date ? date > new Date() : true),
+      "Deadline must be in the future"
+    ),
+  image: z.string().url().optional(),
+});
