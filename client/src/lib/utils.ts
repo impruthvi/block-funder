92e089936e2f363f650347af727bd9bb70188c51
impruthvi <pruthvirajput97@gmail.com>
2025-01-02 12:38:24 +0530

    add utility function to calculate remaining days until deadline

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const daysLeft = (deadline: number) => {
+  const today = new Date().getTime();
+  const remainingDays = Math.floor((deadline - today) / (1000 * 60 * 60 * 24));
+  return remainingDays;
+}
